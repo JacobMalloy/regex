6df114aa54cbf53e877138acbc13cd8cbe76ec00
Jacob Malloy <jmalloy1@vols.utk.edu>
2023-07-29 16:20:04 -0400

    Setup a test file so I can be sure not to loose functionality

---
+++ tests/test.c
@@ -0,0 +1,38 @@
+#include "regex.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void test(char * regex, char *string, struct nfa_node *head, int should_pass){
+    int should_free = 0;
+    if(!head){
+        should_free = 1;
+        head = compile(regex);
+    }
+    if (match(head,string)!=should_pass){
+        regex_free(head);
+        fprintf(stderr,"Failed!\nstring:%s\nregex:%s\n%s\n\n",string,regex,!should_pass?"Did not match":"Matched");
+        exit(1);
+    }else{
+        fprintf(stderr,"Passed!\nstring:%s\nregex:%s\n%s\n\n",string,regex,should_pass?"Did not match":"Matched");
+    }
+    if(should_free){
+        regex_free(head);
+    }
+}
+
+int main(){
+    test("ab","cab",NULL,1);
+    test("abc","cab",NULL,0);
+    test("[ab]","a",NULL,1);
+    test("[ab]*","",NULL,1);
+    test("[ab]*","baba",NULL,1);
+    test("[ab]+","baba",NULL,1);
+    test("[ab]+","",NULL,0);
+    test("[a-zA-Z]+","abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",NULL,1);
+    test("[a-z]+","1",NULL,0);
+    fprintf(stderr,"\n\nAll Tests Passed\n");
+
+    return 0;
+}
